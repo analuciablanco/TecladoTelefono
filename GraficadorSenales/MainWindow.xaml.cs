3305864f25f98e9996584ccf2e3466231e378960
Ana Lucia Blanco <analuciablanco@users.noreply.github.com>
2019-02-01 12:40:43 -0700

    Aún falta por terminar.

--- GraficadorSenales/MainWindow.xaml.cs
+++ GraficadorSenales/MainWindow.xaml.cs
@@ -126,44 +126,146 @@ namespace GraficadorSenales
                 foreach (Muestra muestra in transformada.Muestras)
                 {
                     //Se va graficando la transformada.
-                    plnGraficaResultado.Points.Add(new Point((muestra.x - transformada.TiempoInicial) * scrResultadoOperacion.Width,
-                        (muestra.y / transformada.amplitudMaxima) * ((scrResultadoOperacion.Height / 2.0) - 30) * -1 + (scrResultadoOperacion.Height / 2)));
+                    plnGraficaResultado.Points.Add(new Point(
+                                    (muestra.x - transformada.TiempoInicial) * scrResultadoOperacion.Width,
+                                    (muestra.y / transformada.amplitudMaxima) * ((scrResultadoOperacion.Height / 2.0) - 30) * -1 + (scrResultadoOperacion.Height / 2)
+                                                            ));
                 }
 
-                double valorMaximo = 0;
-                int indiceMaximo = 0;
-                int indiceActual = 0;
+                double valorMaximo_1 = 0;
+                double valorMaximo_2 = 0;
 
-                //Recorrer una colección o arreglo.
+                int indiceMaximo_1 = 0;
+                int indiceMaximo_2 = 0;
+
+                int indiceActual_1 = 0;
+                int indiceActual_2 = 0;
+
+                //Recorrer una colección o arreglo (VALOR MÁXIMO 1).
                 foreach (Muestra muestra in transformada.Muestras)
                 {
                     //Buscamos el valor máximo y el indice actual.
-                    if (muestra.y > valorMaximo)
+                    if (muestra.y > valorMaximo_1)
                     {
-                        valorMaximo = muestra.y;
-                        indiceMaximo = indiceActual;
+                        valorMaximo_1 = muestra.y;
+                        indiceMaximo_1 = indiceActual_1;
                     }
 
-                    indiceActual++;
+                    indiceActual_1++;
 
                     //Evaluamos en la primera mitad de la gráfica de la transformada.
-                    if (indiceActual > (double)transformada.Muestras.Count/2.0)
+                    if (indiceActual_1 > (double)transformada.Muestras.Count/2.0)
                     {
                         break;
                     }
                 }
 
-                double frecuenciaFundamental = (double)indiceMaximo * senal.FrecMuestreo / (double)transformada.Muestras.Count;
-                lblHz.Text = frecuenciaFundamental.ToString() + " Hz";
-
-
-                double indiceMax = (frecuenciaFundamental * senal.FrecMuestreo) / (double)transformada.Muestras.Count;
-                lbl_indiceMax.Text = indiceMax.ToString() + " índice máximo";
-
-                for (int i = 0; i < length; i++)
+                //Recorrer una colección o arreglo (VALOR MÁXIMO 2).
+                foreach (Muestra muestra in transformada.Muestras)
                 {
+                    //Buscamos el valor máximo y el indice actual.
+                    if (muestra.y > valorMaximo_2 && muestra.y < valorMaximo_1)
+                    {
+                        valorMaximo_2 = muestra.y;
+                        indiceMaximo_2 = indiceActual_2;
+                    }
 
+                    indiceActual_2++;
+
+                    //Evaluamos en la primera mitad de la gráfica de la transformada.
+                    if (indiceActual_2 > (double)transformada.Muestras.Count / 2.0)
+                    {
+                        break;
+                    }
                 }
+
+
+                double frecuenciaFundamental_1 = (double)indiceMaximo_1 * senal.FrecMuestreo / (double)transformada.Muestras.Count;
+                double frecuenciaFundamental_2 = (double)indiceMaximo_2 * senal.FrecMuestreo / (double)transformada.Muestras.Count;
+
+                double Valor1 = 0;
+                double Valor2 = 0;
+
+                // Redondear para dar valores exactos.
+                // Valor 1 (Números mayores)
+                if (frecuenciaFundamental_1 >= 1200 && frecuenciaFundamental_1 < 1335) Valor1 = 1209;
+                else if (frecuenciaFundamental_1 >= 1335 && frecuenciaFundamental_1 < 1476) Valor1 = 1336;
+                else if (frecuenciaFundamental_1 >= 1476 && frecuenciaFundamental_1 < 1500) Valor1 = 1477;
+
+                // Valor 2 (Números menores)
+                if (frecuenciaFundamental_2 >= 690 && frecuenciaFundamental_2 < 769) Valor2 = 697;
+                else if (frecuenciaFundamental_2 >= 769 && frecuenciaFundamental_2 < 851) Valor2 = 770;
+                else if (frecuenciaFundamental_2 >= 851 && frecuenciaFundamental_2 < 940) Valor2 = 852;
+                else if (frecuenciaFundamental_2 >= 940 && frecuenciaFundamental_2 < 945) Valor2 = 941;
+
+
+                lblHz_1.Text = frecuenciaFundamental_1.ToString() + " Hz";
+                lblHz_2.Text = frecuenciaFundamental_2.ToString() + " Hz";
+
+                // Se hace la comparación de frecuencias para determinar qué tecla se selecionó.
+                if (Valor1 == 1209 && Valor2 == 697)
+                {
+                    lbl_TeclaValor.Text = "1";
+                }
+                else
+                if (Valor1 == 1336 && Valor2 == 697)
+                {
+                    lbl_TeclaValor.Text = "2";
+                }
+                else
+                if (Valor1 == 1477 && Valor2 == 697)
+                {
+                    lbl_TeclaValor.Text = "3";
+                }
+                else
+                //
+                if (Valor1 == 1209 && Valor2 == 770)
+                {
+                    lbl_TeclaValor.Text = "4";
+                }
+                else
+                if (Valor1 == 1336 && Valor2 == 770)
+                {
+                    lbl_TeclaValor.Text = "5";
+                }
+                else
+                if (Valor1 == 1477 && Valor2 == 770)
+                {
+                    lbl_TeclaValor.Text = "6";
+                }
+                else
+                //
+                if (Valor1 == 1209 && Valor2 == 852)
+                {
+                    lbl_TeclaValor.Text = "7";
+                }
+                else
+                if (Valor1 == 1336 && Valor2 == 852)
+                {
+                    lbl_TeclaValor.Text = "8";
+                }
+                else
+                if (Valor1 == 1477 && Valor2 == 852)
+                {
+                    lbl_TeclaValor.Text = "9";
+                }
+                else
+                //
+                if (Valor1 == 1209 && Valor2 == 941)
+                {
+                    lbl_TeclaValor.Text = "*";
+                }
+                else
+                if (Valor1 == 1336 && Valor2 == 941)
+                {
+                    lbl_TeclaValor.Text = "0";
+                }
+                else
+                if (Valor1 == 1477 && Valor2 == 941)
+                {
+                    lbl_TeclaValor.Text = "#";
+                }
+                else lbl_TeclaValor.Text = "No se detecta ):";
             }
 
             //Graficando el eje de X
